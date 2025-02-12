b47a7a99a796776bfc0c03178ae095d7357839a2
野口瑠星 <noguchi.ryusei@k-idea.jp>
2025-02-12 17:24:23 +0900

    auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyapp339ccba6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "appGroupRole": "string"
+    }
+  }
+}
