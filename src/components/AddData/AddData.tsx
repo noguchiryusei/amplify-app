bf8c0e2004d5e32d3ce4bedcfb20665458eba581
野口瑠星 <noguchi.ryusei@k-idea.jp>
2025-02-18 14:45:32 +0900

    パッケージと画像圧縮の追加

--- src/components/AddData/AddData.tsx
+++ src/components/AddData/AddData.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import conf from '../../assets/aws-exports'
 import { Amplify } from 'aws-amplify'
 import { generateClient } from 'aws-amplify/api';
-import ResizeImage from 'resize-image';
 import {
   Button,
   Flex,
@@ -14,42 +13,26 @@ import {
 } from "../../graphql/mutations";
 import { uploadData } from 'aws-amplify/storage';
 
+import { compressImage } from "../../util/compress.ts"
+
 Amplify.configure(conf)
 const client = generateClient();
 
 const AddData: React.FC = () => {
-  const styles = {
-    inputFile: {
-      '@media (maxWidth: 390px)': {
-        width: '100%',
-      },
-    },
-  };
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
     const image = form.get("image");
-    const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
 
-    if (image && image.size > MAX_IMAGE_SIZE) {
-      // Handle error: image size exceeds maximum limit
-      // Resize the image to a smaller size
-      const resizedImage = await ResizeImage.resizeImage(image, MAX_IMAGE_SIZE);
-      if (resizedImage) {
-        data.image = resizedImage;
-      } else {
-        return;
-      }
-    }
     const data = {
       name: form.get("name"),
       description: form.get("description"),
       image: image.name,
     };
-    if (!!data.image) await uploadData({
+    if (!!data.image) uploadData({
       key: data.name,
-      data: image
+      data: await compressImage(image),
     });
     await client.graphql({
       query: createNoteMutation,
---
+++ src/util/compress.ts
@@ -0,0 +1,28 @@
+import imageCompression from "browser-image-compression";
+
+export type compressImageType = {
+  maxSizeMB: number;
+  maxWidthOrHeight?: number;
+  useWebWorker: boolean;
+  initialQuality: number;
+};
+
+// optionsは必要に応じて調整する
+// https://github.com/Donaldcwl/browser-image-compression#api
+export const compressImage = async (
+  file: File,
+  options: compressImageType = {
+    maxSizeMB: 0.05,
+    useWebWorker: true,
+    initialQuality: 0.75,
+    maxWidthOrHeight: 250,
+  }
+): Promise<File> => {
+  try {
+    const compressedFile = await imageCompression(file, options);
+    return compressedFile;
+  } catch (err) {
+    return Promise.reject(new Error(`compress failed: ${err}`));
+  }
+};
+
