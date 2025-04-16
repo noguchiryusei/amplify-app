1307e669d782cbae565977861546471dd97b5ffa
野口瑠星 <noguchi.ryusei@k-idea.jp>
2025-04-16 14:00:44 +0900

    バックエンドが変わった

--- src/assets/aws-exports.js
+++ src/assets/aws-exports.js
@@ -24,10 +24,10 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://7picitlwaveqpi4bc2zuv7ajcq.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://ux5ote6iarhcvbsjtiqfbh76au.appsync-api.ap-northeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-u4kjfaz5ijbrndlatbuvlfy764",
+    "aws_appsync_apiKey": "da2-esquyjwlczcupm2p4qzsavugki",
     "aws_user_files_s3_bucket": "amplify-app-athome66a31-dev",
     "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
